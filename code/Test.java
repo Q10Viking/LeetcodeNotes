bd965c7a1f8b9940d979eb6eaabe333ef465dce6
q10viking <1403090523@cau.edu.cn>
2019-03-10 22:36:21 +0800

    a

---
+++ code/Test.java
@@ -0,0 +1,8 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Test{
+    public static void main(String[] args){
+        a = 12/10;
+        System.out.println(12/10);
+    }
+}
